c07fa87961c24a9b79efeb9804669de12bde2037
Brandon Burgess <bburge14@uncc.edu>
2019-11-07 15:58:52 +0000

    CSS now works and pages are linked

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'user/login'
   get 'sub/list'
   get 'sub/add'
+  get 'page/contact'
 
   root 'sub#list'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
