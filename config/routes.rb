4ad36f7983f85a5d513c7e2d1e66d51bc1b8a8e9
Brandon Burgess <bburge14@uncc.edu>
2019-11-26 16:06:44 +0000

    Added images

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'sub/add'
   get 'page/contact'
   get 'page/submit'
+  get 'assets/images'
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
