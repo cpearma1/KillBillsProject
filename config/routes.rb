0febee1a5e4a1d0c72f0dbab95a283fb1f3080e8
Brandon Burgess <bburge14@uncc.edu>
2019-11-26 15:27:55 +0000

    Submit page added

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'sub/list'
   get 'sub/add'
   get 'page/contact'
+  get 'page/submit'
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
