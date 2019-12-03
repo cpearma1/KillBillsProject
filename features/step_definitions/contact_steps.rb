ef87455d3a3aec04435a50b9bf9f941669c3a252
Luke <gus.wagner99@gmail.com>
2019-12-03 17:55:14 +0000

    Contact cucumebr test files. Error with submit button on contact_steps.rb

--- features/step_definitions/contact_steps.rb
+++ features/step_definitions/contact_steps.rb
@@ -6,6 +6,10 @@ And ("I see the {string} field") do |string|
     expect(page).to have_field(string)
 end
 
-When ("I click on the submit button") do
-    click_button string
+When ("I click on the {string} button") do
+    click_link string
+end
+
+Then ("I will be on the submit page") do
+    visit page_submit_path
 end
