5895b49764cbc49ab43abe4def4165d43b2f9a9f
Caleb Pearman <caleb.a.pearman@gmail.com>
2019-12-07 02:13:02 -0500

    refactored navbar to purge the nugget and changed root view to the view I've been working on

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  get 'page/about'
+  get 'page/about' => 'page#about'
   get 'user/login'
   get 'sub/list'
   get 'sub/add'
   get 'page/contact' => 'page#contact'
-  get 'page/submit' => 'page#about'
+  get 'page/submit'
   get 'assets/images'
 
 
   resources :subscriptions
 
-  root 'welcome#index'
+  root 'subscriptions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   #post 'user', to: 'controller#action'
-  
- 
-  
+
+
+
 end
