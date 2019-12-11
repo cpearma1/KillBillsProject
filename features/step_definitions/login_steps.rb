072fd87b8572a80070e35c3ff0e30585672e9886
Luke <gus.wagner99@gmail.com>
2019-12-11 19:51:38 +0000

    Removed Merge/head code from login cucumber feature and steps

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -10,16 +10,15 @@ And ("I should see the psw field") do |string|
     expect(page).to have_field(string)
 end
 
-<<<<<<< HEAD
 Then ("I click the the {string} button") do
    expect(page).to have_button(string)
 end
 
 And ("I should be on the {string} page") do
     expect(page).be sub_list_path
-=======
+end
+
 And("I click the {string} button") do |string|
   expect(page).to have_button(string)
->>>>>>> 9681725ad9d3e7554157ab8d121b98a11f7b8c19
 end
 
